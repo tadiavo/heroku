0474caa3eebfc4dbaa0d12c6ce113a153f1b8641
tadiavo <71701241+tadiavo@users.noreply.github.com>
2021-09-07 16:52:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello new new world!'
+    return 'Hello root world!'
+
+@app.route('/test')
+def hello():
+    return 'Hello test!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
